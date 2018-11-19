65d88af5688de54356021562c0d0561d344f2417
Jialiang <jl262@uw.edu>
2018-11-18 23:15:07 -0800

    hehe

--- MergeUniversity.java
+++ MergeUniversity.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 
 public class MergeUniversity {
    public static void main(String[] args) throws FileNotFoundException {
-      Scanner schoolInfo = new Scanner(new File("/Users/Jialiang/Desktop/schoolInfo.csv"));
-      Scanner cwur = new Scanner(new File("/Users/Jialiang/Desktop/cwurData.csv"));
-      schoolInfo.useDelimiter(",");
-      while (schoolInfo.hasNextLine()) {
-         System.out.println(schoolInfo.nextLine() + " | ");
-      }
-      schoolInfo.close();
+      // Scanner schoolInfo = new Scanner(new File("/Users/Jialiang/Desktop/schoolInfo.csv"));
+      // Scanner cwur = new Scanner(new File("/Users/Jialiang/Desktop/cwurData.csv"));
+      // schoolInfo.useDelimiter(",");
+      // while (schoolInfo.hasNextLine()) {
+      //    System.out.println(schoolInfo.nextLine() + " | ");
+      // }
+      // schoolInfo.close();
+      String result = stringHandle("Purdue University--West Lafayette");
+      System.out.println(result);
+   }
+
+   public static String stringHandle(String str) {
+      String output = str.replaceAll("[-,]", " ").trim().replaceAll(" +", " ");;
+      return output; 
    }
 }
