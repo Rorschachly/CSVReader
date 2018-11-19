ed2dfd314f858c229c2839664f9b507498fb833d
Jialiang <jl262@uw.edu>
2018-11-19 13:42:15 -0800

    hehe

--- MergeUniversity.java
+++ MergeUniversity.java
@@ -4,15 +4,28 @@ import java.util.Scanner;
 
 public class MergeUniversity {
    public static void main(String[] args) throws FileNotFoundException {
-      // Scanner schoolInfo = new Scanner(new File("/Users/Jialiang/Desktop/schoolInfo.csv"));
-      // Scanner cwur = new Scanner(new File("/Users/Jialiang/Desktop/cwurData.csv"));
-      // schoolInfo.useDelimiter(",");
-      // while (schoolInfo.hasNextLine()) {
-      //    System.out.println(schoolInfo.nextLine() + " | ");
-      // }
-      // schoolInfo.close();
-      String result = stringHandle("Purdue University--West Lafayette");
-      System.out.println(result);
+      Scanner schoolInfo = new Scanner(new File("/Users/Jialiang/Desktop/CSVReader/schoolInfo.csv"));
+      
+      schoolInfo.useDelimiter(",");
+      int count = 0;
+      while (schoolInfo.hasNextLine()) {
+         String currentLine = schoolInfo.nextLine();
+         currentLine = currentLine.substring(0, currentLine.indexOf(','));
+         currentLine = stringHandle(currentLine);
+         Scanner cwur = new Scanner(new File("/Users/Jialiang/Desktop/CSVReader/cwurData2.csv"));
+         cwur.useDelimiter(",");
+         while (cwur.hasNextLine()) {
+            String cLine = cwur.nextLine();
+            cLine = cLine.substring(0, cLine.indexOf(','));
+            cLine = stringHandle(cLine);
+            if (cLine.equals(currentLine)) {
+               count++;
+               System.out.println(cLine);
+            }
+         }
+      }
+      System.out.println(count);
+      schoolInfo.close();
    }
 
    public static String stringHandle(String str) {
